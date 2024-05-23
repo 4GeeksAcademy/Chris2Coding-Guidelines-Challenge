b75daa0632494d7be9d638ea946acc6d48f45d38
ChrisKeith01 <165314148+ChrisKeith01@users.noreply.github.com>
2024-05-23 00:58:59 +0000

    CompleteGuidelineChallenge

--- src/js/app.js
+++ src/js/app.js
@@ -32,20 +32,33 @@ function render(variables = {}) {
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <img src="${variables.avatarURL}" class="photo" />
+            <h1>${variables.name ? variables.name : "First"} ${
+    variables.lastName ? variables.lastName : "Last"
+  }</h1>
+          <h2>${variables.role ? variables.role : "Role"}</h2>
+            <h3>${variables.city ? variables.city : "City"}, ${
+    variables.country ? variables.country : "Country"
+  }</h3>
+            <ul class="${
+              variables.socialMediaPosition == "position-left"
+                ? "position-left"
+                : "position-right"
+            }">
+              <li><a href="https://twitter.com/${
+                variables.twitter
+              }"><i class="fab fa-twitter"></i></a></li>
+              <li><a href="https://github.com/${
+                variables.github
+              }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${va}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
 }
-
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
@@ -58,6 +71,7 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
+
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
